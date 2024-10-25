a8abd7a1711e64412ca627bc50d966efcc6f8b27
nagolse <nagorniyoleksandr6@gmail.com>
2024-10-25 14:16:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from aiogram.types import ParseMode
 import os
-import subprocess
 import re
 import sqlite3
 from datetime import datetime
